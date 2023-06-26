ab052f1b9b84d7b72dd42ceee20b6c8d54c54d51
Jayanti2919 <jayanti2919@gmail.com>
2023-06-26 12:52:53 +0530

    added partials and layouts

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var express = require('express')
 var path = require('path')
+var expressLayout = require('express-ejs-layouts')
 
 var app = express();
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(expressLayout)
 
 app.get('/', (req, res)=>{
     res.render('index', {
         name: "Jayanti",
     })
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about')
 })
 
 app.listen(8000, ()=>{console.log("Server running on port 8000")})
